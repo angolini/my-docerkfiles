6711ed79891aae7fb28f4d6f50142e37af603b5a
Daiane Angolini <angolini@gmail.com>
2018-08-29 16:33:31 -0300

    improve the script
    Signed-off-by: Daiane Angolini <angolini@gmail.com>

--- download_imx-4.9.88-2.0.0_ga.sh
+++ download_imx-4.9.88-2.0.0_ga.sh
@@ -1,8 +1,20 @@
 #/bin/bash
 
-echo "mkdir imx-4.9.88-2.0.0_ga"
-mkdir imx-4.9.88-2.0.0_ga
-echo "cd imx-4.9.88-2.0.0_ga"
-cd imx-4.9.88-2.0.0_ga
+DIR=yocto-imx-bsp
+
+sudo chown user:user $DIR
+
+echo "mkdir $DIR/imx-4.9.88-2.0.0_ga"
+mkdir -p $DIR/imx-4.9.88-2.0.0_ga
+echo "cd $DIR/imx-4.9.88-2.0.0_ga"
+cd $DIR/imx-4.9.88-2.0.0_ga
+
 repo init -u https://source.codeaurora.org/external/imx/imx-manifest  -b imx-linux-rocko -m imx-4.9.88-2.0.0_ga.xml
 repo sync
+
+cat README-IMXBSP | head
+
+echo -e "\n\n\n"
+
+echo "cat README-IMXBSP to see the complete the options on how to build"
+
